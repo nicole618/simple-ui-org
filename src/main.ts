e070665d6dea70f4b6383d41271b6bbbff792ec9
nicole <1251501326@qq.com>
2021-03-17 11:18:34 +0800

    初始化路由

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
+import Frank from './components/Frank.vue'
+import Frank2 from './components/Frank2.vue'
+import './index.css'
+
+const history = createWebHistory()
+const router = createRouter({
+    history: history,
+    routes: [
+        { path: '/', component: Frank },
+        { path: '/xxx', component: Frank2 }
+    ]
+});
+const app = createApp(App);
+app.use(router)
+app.mount('#app')
+
+
