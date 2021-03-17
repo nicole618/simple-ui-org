bdfa6e67b63f349080665edc5e55c54827651379
nicole <1251501326@qq.com>
2021-03-17 21:59:14 +0800

    button支持level

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -15,14 +15,19 @@ export default {
     size:{
         type: String,
         default: "normal"
+    },
+    level:{
+        type:String,
+        default:'normal'
     }
   },
   setup(props){
-      const {theme,size} = props
+      const {theme,size,level} = props
       const classes = computed(()=>{
           return {
               [`simple-theme-${theme}`]:theme,
-              [`simple-size-${size}`]:size
+              [`simple-size-${size}`]:size,
+              [`simple-level-${level}`]:level
           };
       })
       return {classes}
@@ -36,6 +41,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .simple-button {
   box-sizing: border-box;
   height: $h;
@@ -50,6 +56,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -89,6 +96,53 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.simple-theme-button {
+    &.simple-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.simple-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.simple-theme-link {
+    &.simple-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.simple-theme-text {
+    &.simple-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.simple-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
--- src/views/ButtonDemo.vue
+++ src/views/ButtonDemo.vue
@@ -1,31 +1,49 @@
 <template>
   <div>
     <div>Button示例</div>
-    <h1>示例1</h1>
+    <h2>示例1</h2>
     <div>
       <Button>你好</Button>
       <Button theme="button">你好</Button>
       <Button theme="link">你好</Button>
       <Button theme="text">你好</Button>
     </div>
-    <h1>示例2</h1>
+    <h2>示例2</h2>
+    <div>
     <div>
       <Button size="big">你好</Button>
       <Button>你好</Button>
       <Button size="small">你好</Button>
     </div>
-    <h1>示例3</h1>
     <div>
       <Button size="big" theme="link">你好</Button>
       <Button theme="link">你好</Button>
       <Button size="small" theme="link">你好</Button>
     </div>
-    <h1>示例4</h1>
     <div>
       <Button size="big" theme="text">你好</Button>
       <Button theme="text">你好</Button>
       <Button size="small" theme="text">你好</Button>
     </div>
+    </div>
+    <h2>示例3</h2>
+    <div>
+        <div>   
+     <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text" level="danger">危险文字按钮</Button>
+    </div>
+  </div>
   </div>
 </template>
 
