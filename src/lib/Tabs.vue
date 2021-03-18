5a6f80e6532763ddc621d035b5d638c99c5aeeec
nicole <1251501326@qq.com>
2021-03-18 12:51:02 +0800

    检查Tabs子组件的类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,18 @@
 <template>
   <div>Tabs组件</div>
 </template>
+
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+  components: { Tab },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tab) => {
+      if (tab.type !== Tab) {
+        throw new Error("Tabs的子组件只能是Tab");
+      }
+    });
+  },
+};
+</script>
