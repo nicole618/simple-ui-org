d093273b8725530dd769a8731a627d577ac36f7b
nicole <1251501326@qq.com>
2021-03-18 11:07:37 +0800

    Dialog组件切换按钮完成

--- src/views/DialogDemo.vue
+++ src/views/DialogDemo.vue
@@ -1,11 +1,21 @@
 <template>
   <h2>示例一</h2>
-  <Dialog />
+  <Button @click="toggle">切换Dialog</Button>
+  <Dialog :visible="x" />
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
 import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
 export default {
-  components: { Dialog },
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    return { x, toggle };
+  },
 };
 </script>
