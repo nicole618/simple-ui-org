7e86177f386bfb8b34126b31fd450169d92438db
nicole <1251501326@qq.com>
2021-03-19 11:15:39 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -6,7 +6,9 @@ import DocDemo from './views/DocDemo.vue';
 import Home from './views/Home.vue';
 import SwitchDemo from './views/SwitchDemo.vue';
 import TabsDemo from './views/TabsDemo.vue';
-
+import Intro from './views/Intro.vue'
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
 const history = createWebHistory()
 export const router = createRouter({
     history: history,
@@ -14,7 +16,10 @@ export const router = createRouter({
         { path: '/', component: Home },
         {
             path: '/doc', component: Doc, children: [
-                { path: '', component: DocDemo },
+                { path: 'get-started', component: GetStarted },
+                { path: 'intro', component: Intro },
+                { path: 'install', component: Install },
+                { path: '/', component: DocDemo },
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,21 +3,47 @@
     <TopNav class="nav" :toggleMenuVisible="true" />
     <div class="content">
       <aside v-if="asideVisable">
-        <h2>组件列表</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/switch">Switch 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/button">Button 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/dialog">Dialog 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/tabs">Tabs 组件</router-link>
-          </li>
-        </ol>
+        <ul>
+          <h2 @click="showTowOl">
+            <span>开发指南</span
+            ><svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-bottom"></use>
+            </svg>
+          </h2>
+          <ol>
+            <li>
+              <router-link to="/doc/intro">介绍</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/Install">开始</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/get-started">使用</router-link>
+            </li>
+          </ol>
+        </ul>
+        <ul>
+          <h2 @click="showTowOl">
+            <span>组件</span
+            ><svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-bottom"></use>
+            </svg>
+          </h2>
+          <ol>
+            <li>
+              <router-link to="/doc/switch">Switch 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/button">Button 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/dialog">Dialog 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/tabs">Tabs 组件</router-link>
+            </li>
+          </ol>
+        </ul>
       </aside>
       <main>
         <router-view />
@@ -33,7 +59,16 @@ export default {
   components: { TopNav },
   setup() {
     const asideVisable = inject<Ref<boolean>>("asideVisable");
-    return { asideVisable };
+    const showTowOl = (e: MouseEvent) => {
+      const h2 = e.currentTarget as HTMLElement;
+      const ul = h2.parentNode as HTMLElement;
+      if (ul.classList.contains("openOl")) {
+        ul.classList.remove("openOl");
+      } else {
+        ul.classList.add("openOl");
+      }
+    };
+    return { showTowOl, asideVisable };
   },
 };
 </script>
@@ -48,8 +83,8 @@ export default {
   }
   > .content {
     flex-grow: 1;
-    padding-top: 60px;
-    padding-left: 156px;
+    padding-top: 70px;
+    padding-left: 180px;
     @media (max-width: 500px) {
       padding-left: 0;
     }
@@ -66,25 +101,53 @@ export default {
   }
 }
 aside {
-  background: lightblue;
-  width: 150px;
-  padding: 16px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+  width: 180px;
   position: fixed;
   top: 0;
   left: 0;
-  padding-top: 70px;
+  padding-top: 80px;
   height: 100%;
-  > h2 {
-    margin-bottom: 4px;
-  }
-  > ol {
-    > li {
-      padding: 4px 0;
+  ul {
+    > h2 {
+      line-height: 40px;
+      font-size: 16px;
+      position: relative;
+      padding: 0 15px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      cursor: pointer;
+      .icon {
+        font-size: 20px;
+        transition: all 0.3s ease;
+      }
+    }
+    > ol {
+      display: none;
+      > li {
+        a {
+          display: block;
+          padding-left: 25px;
+        }
+        line-height: 35px;
+      }
+    }
+    &.openOl {
+      h2 {
+        .icon {
+          transform: rotate(180deg);
+        }
+      }
+      ol {
+        display: block;
+      }
     }
   }
+
   @media (max-width: 500px) {
     position: fixed;
-    padding-top: 70px;
+    padding-top: 80px;
     top: 0;
     left: 0;
   }
