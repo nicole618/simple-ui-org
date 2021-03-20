616a0c8a3d43e430dede260ef795ba3ed5a32b58
nicole <1251501326@qq.com>
2021-03-20 11:06:57 +0800

    优化Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -6,13 +6,12 @@
       <component :is="component" />
     </div>
     <div class="format-code">
-      <div class="format-code-text">
-        <pre
-          class="language-html"
-          v-html="Prism.highlight(component.__sourceCode, Prism.languages.html)"
-        ></pre>
+      <div class="format-code-text" v-if="codeVisible">
+        <pre class="language-html" v-html="html"></pre>
       </div>
-      <p class="format-code-btn">显示代码</p>
+      <p class="format-code-btn" @click="codeVisible = !codeVisible">
+        显示代码
+      </p>
     </div>
   </section>
 </template>
@@ -21,14 +20,24 @@
 import "prismjs";
 import "prismjs/themes/prism-solarizedlight.css";
 const Prism = (window as any).Prism;
+import { computed, ref } from "vue";
 export default {
   props: {
     component: Object,
     componentStr: String,
   },
-  setup() {
+  setup(props) {
+    const html = computed(() => {
+      return Prism.highlight(
+        props.component.__sourceCode,
+        Prism.languages.html
+      );
+    });
+    const codeVisible = ref(false);
     return {
       Prism,
+      html,
+      codeVisible,
     };
   },
 };
