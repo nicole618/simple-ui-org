7f9e8d8ff55f6e79502de51a0fd994572238dbbf
nicole <1251501326@qq.com>
2021-03-21 19:58:42 +0800

    将svg放到本地

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
 import 'github-markdown-css'
+import './assets/svg.js'
 
 const app = createApp(App);
 app.use(router)
