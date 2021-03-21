759a9a66f3a99b4101cd3461187a7f47393338bc
nicole <1251501326@qq.com>
2021-03-21 22:33:41 +0800

    修改md

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:nicole618/simple-ui.git &&
+git remote add origin git@gitee.com:nicole618/simple-ui.git &&
 git push -f -u origin master &&
 cd -
 echo https://nicole618.github.io/simple-ui/index.html 
